48d108addd7dc4e6d7f22b19acdb51f08585f8c0
Manya <mayavi1319@gmail.com>
2025-02-19 14:37:48 +0530

    completed assignment 2

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const { resolve } = require('path');
+const connectToDb = require('./static/db');
+const userRouter = require('./schema');
+require('dotenv').config()
 
 const app = express();
+app.use(express.json())
 const port = 3010;
+const db_url = process.env.MONGO_URI;
+
 
 app.use(express.static('static'));
 
@@ -10,6 +16,16 @@ app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.use('/api', userRouter)
+
+app.listen(port, async() => {
+  try{
+
+    await connectToDb(db_url)
+    console.log(`Example app listening at http://localhost:${port}`);
+    console.log('Connected to database')
+  }
+  catch(err){
+    console.log(err)
+  }
 });
--- schema.js
+++ schema.js
@@ -1,4 +1,9 @@
+require ('dotenv').config();
 const mongoose = require('mongoose');
+const express = require('express');
+
+const router = express.Router()
+// const {body, validationResult} = require("express-validator")
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -20,6 +25,21 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
+router.post('/users', async (req, res) => {
+  try{
+    const {name, email, password} = req.body;
+    const newUser = new User({name, email, password});
+    await newUser.validate();
+    await newUser.save();
+    res.status(201).json({message: "User created successfully", user: newUser});
+  } catch(error){
+    res.status(500).json({error: error.message});
+  }
+  });
+
+  module.exports = router
